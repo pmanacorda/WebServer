6bd3d2c099b8841b7b7bee22d47f68e604b5cd79
Patrick Manacorda <88177272+pmanacorda@users.noreply.github.com>
2025-06-18 13:32:19 -0700

    feature: add about me data (#16)

--- client/about/about.js
+++ client/about/about.js
@@ -3,7 +3,25 @@ window.onload = function(){
     .then(response => {
         return response.json();
     }).then(data => {
-        document.getElementById("text").innerText = data[0].Text;
+        document.getElementById("name").innerText = data[0].name;
+        document.getElementById("birthplace").innerText = data[0].birthplace;
+        document.getElementById("university").innerText = data[0].university;
+        document.getElementById("degree").innerText = data[0].degree;
+        document.getElementById("marital_status").innerText = data[0].marital_status;
+        document.getElementById("current_location").innerText = data[0].current_location;
+        document.getElementById("spouse_occupation").innerText = data[0].spouse_occupation;
+        document.getElementById("hobby_1").innerText = data[0].hobby_1;
+        document.getElementById("hobby_2").innerText = data[0].hobby_2;
+        document.getElementById("hobby_3").innerText = data[0].hobby_3;
+        document.getElementById("favorite_movie_series_1").innerText = data[0].favorite_movie_series_1;
+        document.getElementById("favorite_movie_series_2").innerText = data[0].favorite_movie_series_2;
+        document.getElementById("favorite_movie_series_3").innerText = data[0].favorite_movie_series_3;
+        document.getElementById("favorite_video_game").innerText = data[0].favorite_video_game;
+        document.getElementById("favorite_game_year").innerText = data[0].favorite_game_year;
+        document.getElementById("favorite_game_publisher").innerText = data[0].favorite_game_publisher;
+        document.getElementById("favorite_game_genre").innerText = data[0].favorite_game_genre;
+        document.getElementById("recent_game_played").innerText = data[0].recent_game_played;
+        document.getElementById("gaming_frequency").innerText = data[0].gaming_frequency;
     })
 }
 
--- server/controllers/AboutController.cpp
+++ server/controllers/AboutController.cpp
@@ -18,10 +18,28 @@ namespace Controllers{
             contentType = "application/javascript";
         }
         else if(req.path == "/api/about" && req.method == Core::HttpMethod::GET){
-            std::unordered_map<std::string, std::string> result;
-            result["Text"] = "test";
+            std::unordered_map<std::string, std::string> aboutMe;
+            aboutMe["name"] = "Patrick Manacorda";
+            aboutMe["birthplace"] = "Vercelli, Italy";
+            aboutMe["university"] = "Seattle University";
+            aboutMe["degree"] = "Bachelor's in Computer Science";
+            aboutMe["marital_status"] = "Married";
+            aboutMe["current_location"] = "Tucson, Arizona";
+            aboutMe["spouse_occupation"] = "Medical student at University of Arizona College of Medicine";
+            aboutMe["hobby_1"] = "Programming";
+            aboutMe["hobby_2"] = "Playing chess";
+            aboutMe["hobby_3"] = "Exercising";
+            aboutMe["favorite_movie_series_1"] = "Star Wars";
+            aboutMe["favorite_movie_series_2"] = "Harry Potter";
+            aboutMe["favorite_movie_series_3"] = "Lord of the Rings";
+            aboutMe["favorite_video_game"] = "Freelancer";
+            aboutMe["favorite_game_year"] = "2003";
+            aboutMe["favorite_game_publisher"] = "Microsoft";
+            aboutMe["favorite_game_genre"] = "Space simulation";
+            aboutMe["recent_game_played"] = "Skyrim";
+            aboutMe["gaming_frequency"] = "Not much anymore";
             res.statusCode = 200;
-            res.body.push_back(std::move(result));
+            res.body.push_back(std::move(aboutMe));
             res.headers["Content-Type"] = "application/json";
             return;
         }
